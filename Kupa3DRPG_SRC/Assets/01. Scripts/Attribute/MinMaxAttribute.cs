d65dc7ba0dbe7da31c65a63d79d44ce259ead9ec
jab724 <jab724@naver.com>
2021-06-10 18:12:33 +0900

    1. 캐릭터 이동

---
+++ Kupa3DRPG_SRC/Assets/01. Scripts/Attribute/MinMaxAttribute.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class MinMaxAttribute : PropertyAttribute
+{
+    public float MinLimit = 0;
+    public float MaxLimit = 1;
+    public bool ShowEditRange;
+    public bool ShowDebugValues;
+
+    public MinMaxAttribute(float min, float max)
+    {
+        MinLimit = min;
+        MaxLimit = max;
+    }
+}
+
+public class MinMaxValue
+{
+    public float min;
+    public float max;
+    public MinMaxValue(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
+public class MinMaxCurrentValue
+{
+    private float min;
+    public float Min
+    {
+        get { return min; }
+        set
+        {
+            min = value;
+            if (current < min) current = min;
+        }
+    }
+    private float current;
+    public float Current
+    {
+        get { return current; }
+        set
+        {
+            current = value;
+            if (current < min) current = min;
+            if (max < current) current = max;
+        }
+    }
+    private float max;
+    public float Max
+    {
+        get { return max; }
+        set
+        {
+            max = value;
+            if (max < current) current = max;
+        }
+    }
+    public MinMaxCurrentValue(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+        this.current = (min + max) * 0.5f;
+    }
+    public MinMaxCurrentValue(float min, float current, float max)
+    {
+        this.min = min;
+        this.current = current;
+        this.max = max;
+    }
+}
---
+++ Kupa3DRPG_SRC/Assets/01. Scripts/WebGLPause.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//Web GL 환경 특성상 마우스가 보이며 게임 진행에 있어 거슬리기에 마우스 포인터가 보이지 않도록 하고 
+//포커스를 잃을 상황이 빈번하여 이에 관한 불편함 최소화
+public class WebGLPause : MonoBehaviour, IPointerDownHandler
+{
+    Canvas canvas;
+
+    private void Awake()
+    {
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SetPause(true);
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        SetPause(!focus);
+    }
+
+    private void SetPause(bool isPause)
+    {
+        if (isPause)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0.0f;
+            canvas.enabled = true;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1.0f;
+            canvas.enabled = false;
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SetPause(false);
+    }
+}
